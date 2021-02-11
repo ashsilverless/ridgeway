9c0b45c7892f04b0aa2db22a3a76e139ca794b3a
Ash - SIlverless <ash@silverless.co.uk>
2021-02-11 11:57:50 +0000

    DEV - target blank, socials

--- template-parts/socials.php
+++ template-parts/socials.php
@@ -1,5 +1,5 @@
 <div class="socials mobile">
     <?php if( have_rows('social_links', 'option') ): while( have_rows('social_links', 'option') ): the_row(); ?>
-    <a href="<?php the_sub_field('page_link'); ?>" class="<?php the_sub_field('name'); ?>"><i class="fab fa-<?php the_sub_field('name'); ?>"></i></a>
+    <a href="<?php the_sub_field('page_link'); ?>" class="<?php the_sub_field('name'); ?>" target="_blank"><i class="fab fa-<?php the_sub_field('name'); ?>"></i></a>
     <?php endwhile; endif; ?>
 </div>
